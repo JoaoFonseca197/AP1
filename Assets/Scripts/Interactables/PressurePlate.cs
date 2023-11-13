cdcf4d2bece625f74062f592dcb49d540a93091c
João Fonseca <joao.hc.fonseca197@gmail.com>
2023-11-13 20:41:20 +0000

    Made basic PressurePlate

--- Assets/Scripts/Interactables/PressurePlate.cs
+++ Assets/Scripts/Interactables/PressurePlate.cs
@@ -1,21 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
-public class PressurePlate : MonoBehaviour , IInteractable
+public class PressurePlate : MonoBehaviour 
 {
-    [SerializeField] private GameObject _objectToActivate;
+    [SerializeField] private Animator _objectToActivate;
     [SerializeField] private Animator _animator;
-
-
+    [SerializeField] private Collider _collider;
     public Characters CurrentCharacter { get; }
-    void IInteractable.Interact(Characters character)
+
+    private bool _isActive = false;
+    private void Awake()
     {
-
-        _animator.parameters.SetValue(true,0);
-
+        _isActive = false; 
     }
 
-    void IInteractable.StopInteract() { }
+    private void OnTriggerEnter(Collider other)
+    {
+        //if(other.GetComponent<Box>())
+        //{
+        //    Debug.Log("Box On");
+        //    _isActive = true;
+        //}
+        Debug.Log("Box On");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(_isActive) 
+        {
+            Debug.Log("Box off");
+            _isActive = false;
+        }
+
+        Debug.Log("Box off");
+    }
+
 
 }
