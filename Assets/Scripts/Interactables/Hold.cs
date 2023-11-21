61f937a472a2d68533d7f834104841c028f967cc
João Fonseca <joao.hc.fonseca197@gmail.com>
2023-11-21 18:25:19 +0000

    Make Hold mechanic

---
+++ Assets/Scripts/Interactables/Hold.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Hold : MonoBehaviour, IInteractable
+{
+    [SerializeField] private Animator   _objectanimator;
+    [SerializeField] private Animator   _animator;
+    [SerializeField] private PlayerController _playerController;
+
+    private Characters _currentCharacter;
+
+    public Characters CurrentCharacter => _currentCharacter;
+
+    void IInteractable.Interact(Characters character)
+    {
+        _animator.SetBool("IsActive", true);
+        _objectanimator.SetBool("IsActive", true);
+        _playerController.CurrentInteractable = null;
+        _currentCharacter = character;
+        _currentCharacter.NavMeshAgent.isStopped = true;
+        _currentCharacter.Interactable = this;
+        
+    }
+
+
+    void IInteractable.StopInteract() 
+    {
+        _animator.SetBool("IsActive", false);
+        _objectanimator.SetBool("IsActive", false);
+        _currentCharacter.Interactable = null;
+        _currentCharacter.NavMeshAgent.isStopped = false;
+        _currentCharacter = null;
+    }
+
+}
