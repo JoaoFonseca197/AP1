86dab56dd0990692d826f05c376cb0f382f1befd
João Fonseca <joao.hc.fonseca197@gmail.com>
2023-11-21 19:46:24 +0000

    Fix bug on switch script

--- Assets/Scripts/Interactables/Switch.cs
+++ Assets/Scripts/Interactables/Switch.cs
@@ -9,23 +9,34 @@ public class Switch : MonoBehaviour, IInteractable
     [SerializeField] private Animator _animator;
 
     private Characters _currentCharacter;
+    private bool _isActive;
+
+    private void Awake()
+    {
+        _isActive = false; 
+    }
 
     public Characters CurrentCharacter => _currentCharacter;
 
     public void Interact(Characters character)
     {
-        _currentCharacter = character;
-        _animator.SetBool("IsActive", true);
-        _objectanimator.SetBool("IsActive", true);
+        if(_isActive)
+        {
+            _animator.SetBool("IsActive", false);
+            _objectanimator.SetBool("IsActive", false);
+            _isActive = false;
+        }
+        else
+        {
+            _animator.SetBool("IsActive", true);
+            _objectanimator.SetBool("IsActive", true);
+            _isActive = true;
+        }
+        
 
-        _currentCharacter.Interactable = this;
+        
     }
 
 
-    public void StopInteract() 
-    {
-        _animator.SetBool("IsActive", false);
-        _objectanimator.SetBool("IsActive", false);
-        _currentCharacter.Interactable = null;
-    }
+    public void StopInteract() { }
 }
