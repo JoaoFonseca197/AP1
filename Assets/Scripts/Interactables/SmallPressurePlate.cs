04c851b8e3a6c16b333fed769b3bb19804b1165e
João Fonseca <joao.hc.fonseca197@gmail.com>
2023-11-23 23:08:16 +0000

    Implement SmallPressurePlate

--- Assets/Scripts/Interactables/SmallPressurePlate.cs
+++ Assets/Scripts/Interactables/SmallPressurePlate.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class SmallPressurePlate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Animator _objectToActivate;
+    [SerializeField] private Animator _animator;
+    public Characters CurrentCharacter { get; }
+
+    private bool _isActive;
+    private GameObject _currentObjectActivating;
+
+    private void Awake()
     {
-        
+        _isActive = false;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.TryGetComponent<SmallBox>(out SmallBox bigBox))
+            _currentObjectActivating = bigBox.gameObject;
+        else if (other.TryGetComponent<PlayerCharacter>(out PlayerCharacter playerCharacter))
+            _currentObjectActivating = playerCharacter.gameObject;
+        else if(other.TryGetComponent<MasonCharacter>(out MasonCharacter masonCharacter))
+            _currentObjectActivating= masonCharacter.gameObject;
+
+        if (_currentObjectActivating != null)
+        {
+            _animator.SetBool("IsActive", true);
+            _objectToActivate.SetBool("IsActive", true);
+            _isActive = true;
+        }
+
+
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (_isActive && other.gameObject == _currentObjectActivating)
+        {
+            _animator.SetBool("IsActive", false);
+            _objectToActivate.SetBool("IsActive", false);
+            _isActive = false;
+            _currentObjectActivating = null;
+        }
+
     }
 }
